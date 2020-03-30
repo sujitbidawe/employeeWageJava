b4860bc1da3c745155d6ebe03e76d2ad4dd69b9e
sujitbidawe <bidawesujit@gmail.com>
2020-03-30 16:57:36 +0530

    refactored: code to compute employee wage for multiple companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,29 +1,17 @@
 class EmployeeWage{
-	//constants
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
-
-	//variables
-	public static int empHrs = 0;
-	public static int totalEmpHrs = 0;
-	public static int totalWorkingDays = 0;
-		
 	
-public static void main(String[] args) {
-		
-		System.out.println("Welcome to Employee Wage Computation Program!");
+	public void computeWage( String compName, int EMP_RATE_PER_HOUR, int MAX_NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH ){
+		final int IS_PART_TIME = 1;
+		final int IS_FULL_TIME = 2;
 
-		computeWage();
-		
-		
-	}
+		int empHrs = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+	
+		System.out.println("Welcome to "+ compName +"'s data");
 
-	public static void computeWage(){
 		//computation
-		while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS ) {
+		while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < MAX_NUM_OF_WORKING_DAYS ) {
 			totalWorkingDays++;
 			int empCheck = (int)(( Math.random() * 10 ) % 3);
 			switch (empCheck) {
@@ -40,8 +28,20 @@ public static void main(String[] args) {
 			System.out.println("Day# " + totalWorkingDays + "--> Emp Hr: " +  empHrs);
 		}
 		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Emp Wage: " + totalEmpWage);
+		System.out.println("In " + compName + ", total salary of given employee is: " + totalEmpWage );
 		
-
 	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee Wage Computation Program!");
+
+		EmployeeWage employee = new EmployeeWage();
+
+		employee.computeWage("bridgelabz", 20, 20, 100);
+		System.out.println();
+		employee.computeWage("google", 25, 20, 120);
+		
+	}
+
 }
